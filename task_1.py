38af809d312e417dd423d21c36497ddaa569cf8e
YuiSova <tarasovajulia71@gmail.com>
2024-11-13 21:11:00 +0300

    Delete task_1.py

--- task_1.py
+++
@@ -1,7 +0,0 @@
-numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
-index_of_none = 4
-sum_without_none = sum(numbers[:4] + numbers[5:])
-count = len(numbers)
-average = sum_without_none / count
-numbers[index_of_none] = average
-print("Измененный список:",numbers)
